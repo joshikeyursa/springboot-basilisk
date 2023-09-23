94057fbee621e823b648e0cb3b96c2a85c27ea59
keyurjoshi <joshikeyursa@gmail.com>
2023-09-23 15:29:54 +1000

    Initial commit

---
+++ src/main/java/com/kjoshi/sb/basilisk/BasiliskApplication.java
@@ -0,0 +1,13 @@
+package com.kjoshi.sb.basilisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiliskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiliskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjoshi/sb/basilisk/BasiliskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjoshi.sb.basilisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiliskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
